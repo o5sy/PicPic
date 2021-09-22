f931d22d10e4f625d3eb292a42294baed9b5e899
osy <ayul5@naver.com>
2021-09-22 16:34:48 +0900

    Feat: Add 메인페이지 사진 아이템 마우스 오버 시 추가 정보 표시
    - 페이지별로 동일한 ui가 있으면 기능 하나 추가할 때마다 페이지
      별로 html, css, js를 수정해야되는게 비효율적임
      -> 재사용하기 위한 방법을 생각해봐야겠음

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -58,8 +58,33 @@ class Index {
   setPhotoList(photoList) {
     // 각 사진별 상세보기 링크 연결
 
-    photoList.map((data) => {
-      const { id, urls } = data;
+    // photoList.map((data) => {
+    //   const { id, urls } = data;
+
+    //   // 엘리먼트 생성
+    //   const li = document.createElement("li"),
+    //     link = document.createElement("a"),
+    //     img = document.createElement("img");
+
+    //   // 클래스 추가
+    //   li.classList.add("item");
+    //   link.classList.add("item__photo-link");
+
+    //   // 데이터 추가
+    //   link.href = `/photo/${id}`;
+    //   img.src = urls.thumb;
+
+    //   // 노드 연결
+    //   li.appendChild(link);
+    //   link.appendChild(img);
+
+    //   // 부모 노드 연결
+    //   this.photoListView.appendChild(li);
+    // });
+
+    // 사진 목록 추가
+    photoList.map((info) => {
+      const { id, urls, user, links } = info;
 
       // 엘리먼트 생성
       const li = document.createElement("li"),
@@ -77,6 +102,61 @@ class Index {
       // 노드 연결
       li.appendChild(link);
       link.appendChild(img);
+
+      // 유저 정보 연결
+      const infoDiv = document.createElement("div"),
+        userInfoDiv = document.createElement("div"),
+        userProfileImg = document.createElement("img"),
+        userNameDiv = document.createElement("div"),
+        downloadButton = document.createElement("button");
+
+      // 클래스 추가
+      infoDiv.classList.add("info-container");
+      userInfoDiv.classList.add("user-info");
+      userNameDiv.classList.add("user-name");
+      downloadButton.classList.add("download-button");
+
+      // 유저 정보 추가
+      userProfileImg.setAttribute("src", user.profile_image.small);
+      userNameDiv.innerHTML = user.name;
+
+      // 다운로드 기능 추가
+      downloadButton.addEventListener("click", () => {
+        // 이미지 url
+        const downloadUrl = urls.regular;
+
+        // 이미지 명
+        let extension = getParam("fm", "jpg", downloadUrl);
+        let fileName = id + "-regular." + extension;
+
+        // 다운로드
+        var x = new XMLHttpRequest();
+        x.open("GET", downloadUrl, true);
+        x.responseType = "blob";
+        x.onload = function (e) {
+          downloadjs(x.response, fileName, `image/${extension}`);
+        };
+        x.send();
+
+        // 다운로드 추적
+        photoModel.trackDownloadPhoto(links.download_location);
+      });
+
+      // 애니메이션 추가
+      li.addEventListener("mouseover", () => {
+        infoDiv.classList.add("hover");
+      });
+      li.addEventListener("mouseleave", () => {
+        if (!infoDiv.classList.contains("hover")) return;
+        infoDiv.classList.remove("hover");
+      });
+
+      // 노드 연결
+      li.appendChild(infoDiv);
+      infoDiv.appendChild(userInfoDiv);
+      userInfoDiv.appendChild(userProfileImg);
+      userInfoDiv.appendChild(userNameDiv);
+      infoDiv.appendChild(downloadButton);
 
       // 부모 노드 연결
       this.photoListView.appendChild(li);
